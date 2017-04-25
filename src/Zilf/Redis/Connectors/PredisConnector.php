baa923d4110dda5d139c7d8caf06eb6fddd924b4
txj123 <597196313@qq.com>
2017-04-25 14:56:20 +0800

    添加 redis缓存 支持predis和redis

---
+++ src/Zilf/Redis/Connectors/PredisConnector.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Zilf\Redis\Connectors;
+
+use Predis\Client;
+use Zilf\Support\Arr;
+use Zilf\Redis\Connections\PredisConnection;
+use Zilf\Redis\Connections\PredisClusterConnection;
+
+class PredisConnector
+{
+    /**
+     * Create a new clustered Predis connection.
+     *
+     * @param  array  $config
+     * @param  array  $options
+     * @return \Zilf\Redis\Connections\PredisConnection
+     */
+    public function connect(array $config, array $options)
+    {
+        return new PredisConnection(new Client($config, array_merge(
+            ['timeout' => 10.0], $options, Arr::pull($config, 'options', [])
+        )));
+    }
+
+    /**
+     * Create a new clustered Predis connection.
+     *
+     * @param  array  $config
+     * @param  array  $clusterOptions
+     * @param  array  $options
+     * @return \Zilf\Redis\Connections\PredisClusterConnection
+     */
+    public function connectToCluster(array $config, array $clusterOptions, array $options)
+    {
+        $clusterSpecificOptions = Arr::pull($config, 'options', []);
+
+        return new PredisClusterConnection(new Client(array_values($config), array_merge(
+            $options, $clusterOptions, $clusterSpecificOptions
+        )));
+    }
+}
