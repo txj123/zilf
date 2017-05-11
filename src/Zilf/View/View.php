c960e3937329ac93e2d69984d44d007d9d18f973
wuyading <863758424@qq.com>
2017-05-11 17:15:31 +0800

    test

--- src/Zilf/View/View.php
+++ src/Zilf/View/View.php
@@ -15,5 +15,6 @@ class View
         //优化，如果view有后缀名称，则直接按照后缀寻找视图文件，不会添加默认后缀
         $suffix = $this->getParameter('framework.view_suffix');
         $this->defaultExtension = $suffix ?? '.php';
+        echo '';
     }
 }
